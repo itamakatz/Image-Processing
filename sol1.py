bf7d312da0895c1719e6bfa7e193bf95c4ad528e
itamakatz <itamakatz@gmail.com>
2016-11-19 18:53:22 +0200

    Finished task 3.1

--- sol1.py
+++ sol1.py
@@ -1,3 +1,19 @@
 import numpy as np
+from scipy.misc import imread as imread
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
 
-def reaf_image(filename,representation):
+def reaf_image(filename, representation):
+    im = imread(filename)
+    im_float = im.astype(np.float32)
+    im_float /= 255
+
+    if(representation == 1):
+        im_g = rgb2gray(im)
+        return im_g
+
+    return im
+
+image = reaf_image("/home/itamar/Documents/image_processing/ex1/Files/test files/external/jerusalem.jpg", 1)
+plt.imshow(image, plt.cm.gray)
+plt.show()
