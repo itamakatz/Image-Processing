61c05b455d179b050ed115f526d9646c181e7f71
itamakatz <itamakatz@gmail.com>
2016-11-23 20:12:01 +0200

    Finished task 3.4. (may be there are a few problems compared to the facebook sol)

--- sol1.py
+++ sol1.py
@@ -41,86 +41,97 @@ def yiq2rgb(imYIQ):
 
 def histogram_equalize(im_orig):
 
-    # #
-    # temp_im, im = None, None
-    #
-    # # Checking weather it is a B&W image or comor
-    # if(im_orig.ndim == 2):
-    #     im = im_orig
-    # else:
-    #     temp_im = (rgb2yiq(im_orig))
-    #     im = temp_im[:,:,0]
-    #
-    # # Computing histogram of original image
-    # hist_orig, bins = np.histogram(im.flatten(), 256)
-    # #  Compute thr cumulative histogram
-    # cumulative_histogram = np.cumsum(hist_orig)
-    #
-    # # find first m for which S(m) != 0
-    # m_value = (cumulative_histogram[cumulative_histogram > 0])[0]
-    # # applay linear streachin
-    # cumulative_streach = np.round((cumulative_histogram - m_value) * 255 / (cumulative_histogram[-1] - m_value))
-    #
-    # # computing the hist_eq using an empty vector
-    # # calc_hist_vect = np.zeros((257,))
-    # # calc_hist_vect[1:] = cumulative_streach[:]
-    # # hist_eq = (cumulative_streach - calc_hist_vect[:-1])
-    #
-    # la = np.interp(im, bins[:-1], cumulative_streach)
-    #
-    # if(im_orig.ndim == 2):
-    #     im_eq = la / 255
-    # else:
-    #     temp_im[:,:,0] = la / 255
-    #     im_eq = np.clip(yiq2rgb(temp_im), 0 ,1)
-    #
-    # hist_eq, _ = np.histogram(la, 256)
-    #
-    # return [im_eq, hist_orig, hist_eq]
+    yiq = None
 
-    temp_im, im = None, None
-
-    # Checking weather it is a B&W image or comor
+    # check if it is a B&W or color image
     if(im_orig.ndim == 2):
         im = im_orig
     else:
-        temp_im = (rgb2yiq(im_orig))
-        im = temp_im[:,:,0]
+        # transform to the YIQ space
+        yiq = rgb2yiq(im_orig)
+        im = yiq[:, :, 0]
 
-    # Computing histogram of original image
-    hist_orig, bins = np.histogram(im.flatten(), 256)
-    #  Compute thr cumulative histogram
+    # calc the histogram of the image
+    hist_orig, bins = np.histogram(im.flatten(), 255)
+    # compute the cumulative histogram
     cumulative_histogram = np.cumsum(hist_orig)
-
+    # cumulative_histogram = cumulative_histogram / cumulative_histogram[-1] * 255
     # find first m for which S(m) != 0
-    m_value = (cumulative_histogram[cumulative_histogram > 0])[0]
-    # applay linear streachin
-    cumulative_streach = (cumulative_histogram - m_value) * 255 / (cumulative_histogram[-1] - m_value)
-
-    # computing the hist_eq using an empty vector
-    # calc_hist_vect = np.zeros((257,))
-    # calc_hist_vect[1:] = cumulative_streach[:]
-    # hist_eq = (cumulative_streach - calc_hist_vect[:-1])
+    m_val = (cumulative_histogram[cumulative_histogram > 0])[0]
+    # apply linear stretching
+    cumulative_stretch = np.round(255 * (cumulative_histogram - m_val) / (cumulative_histogram[-1] - m_val))
 
     if(im_orig.ndim == 2):
-        im_eq = np.interp(im.flatten(), bins[:-1], cumulative_streach).reshape(im.shape) / 255
+        # apply the look up table to the image
+        im_eq = np.interp(im.flatten(), bins[:-1], cumulative_stretch).reshape(im.shape)
+        #  calc the histogram of the enhanced image
+        hist_eq, bins2 = np.histogram(im_eq, 255)
     else:
-        temp_im[:,:,0] = np.interp(im.flatten(), bins[:-1], cumulative_streach).reshape(im.shape) / 255
-        im_eq = np.clip(yiq2rgb(temp_im), 0 ,1)
-
-    hist_eq, _ = np.histogram(im_eq.flatten(), 256)
+        # apply the look up table to the image
+        yiq[:, :, 0] = np.interp(im.flatten(), np.linspace(0, 1, 255, True), cumulative_stretch).reshape(im.shape) / 255
+        #  ensure the values after the transformation are in the [0,1] range by "clipping"
+        im_eq = np.clip(yiq2rgb(yiq), 0, 1)
+        #  calc the histogram after the clipping
+        # yiq = rgb2yiq(yiq2rgb(im_eq))
+        hist_eq, bins2 = np.histogram(yiq[:, :, 0].flatten(), 255)
 
     return [im_eq, hist_orig, hist_eq]
 
-# imRGB = yiq2rgb(rgb2yiq())
+def histogram_equalize2(im_orig):
+    #######################################################
+    # Performs histogram equalization on a given image,
+    # according to the lecture algorithm.
+    #######################################################
+    isRGB, imYIQ = (len(im_orig.shape) == 3), None
+    if isRGB:
+        imYIQ = rgb2yiq(im_orig)
+        im_orig = imYIQ[:, :, 0]
+    #get the histogram of the image
+    hist_orig, bins = np.histogram(im_orig.flatten(), 255)
+    #compute the cumulative histogram
+    cdf = np.cumsum(hist_orig)
+    #normalize the cumulative histogram
+    cdf = np.round(255 * (cdf - cdf[cdf>0].min()) / (cdf.max() - cdf[cdf>0].min()))
+    #use linear interpolation of cdf to find new pixel values
+    im_eq = np.interp(im_orig, np.linspace(0, 1, 255), cdf)
+    #histogram the new image
+    hist_eq, bins = np.histogram(im_eq.flatten(), 255)
+    #if we got RGB, return it back to RGB
+    if isRGB:
+        imYIQ[:, :, 0] = im_eq / 255
+        #using clip to zero the negative results of the transformation
+        im_eq = np.clip(yiq2rgb(imYIQ), 0, 1)
+    return im_eq, hist_orig, hist_eq
+
 
 # # imdisplay("/home/itamar/Documents/image_processing/ex1/Files/test files/external/jerusalem.jpg", 2)
 
-[im_eq, hist_orig, hist_eq] = histogram_equalize(read_image \
-                                ("/home/itamar/Documents/image_processing/ex1/Files/test files/external/monkey.jpg", 2))
-# plt.imshow(im_eq)
+# im_orig = read_image ("/home/itamar/Documents/image_processing/ex1/Files/test files/external/monkey.jpg", 2)
+# im_orig = read_image ("/home/itamar/Documents/image_processing/ex1/Files/test files/external/jerusalem.jpg", 2)
+im_orig = read_image ("/home/itamar/Documents/image_processing/ex1/Files/test files/external/Low Contrast.jpg", 2)
+
+[im_eq, hist_orig, hist_eq] = histogram_equalize(im_orig)
+# [im_eq, hist_orig, hist_eq] = histogram_equalize2(im_orig)
+
+plt.figure(1)
+plt.subplot(221)
+plt.title("Original Image")
+plt.imshow(im_orig)
+# plt.imshow(im_orig, plt.cm.gray)
+
+plt.subplot(222)
+plt.title("Equalized Image")
+plt.imshow(im_eq)
 # plt.imshow(im_eq, plt.cm.gray)
-plt.plot(np.arange(256) ,hist_orig)
-plt.show()
-plt.plot(np.arange(256) ,hist_eq)
+
+plt.subplot(223)
+plt.title("Histograms: Original - Red, Equalized - Blue")
+plt.plot(np.arange(len(hist_orig)) ,hist_orig, 'r', np.arange(len(hist_eq)), hist_eq, 'b')
+
+plt.subplot(224)
+plt.title("PDF Histograms: Original - Red, Equalized - Blue")
+pdf_original = np.cumsum(hist_orig);
+pdf_eq = np.cumsum(hist_eq);
+pdf_eq = pdf_eq / pdf_eq.max() * 255
+plt.plot(np.arange(len(pdf_original)) ,np.arange(len(pdf_original)), 'r', np.arange(len(pdf_eq)) ,pdf_eq, 'b')
 plt.show()
