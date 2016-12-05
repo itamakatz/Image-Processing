7c65237e2b58a27f9ddca0ed710f750e7ebaebfa
itamakatz <itamakatz@gmail.com>
2016-12-06 01:03:57 +0200

    installed jupyter and made first file to use it. as well, i started task 1.1

---
+++ ex2/sol2.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy.misc import imread as imread
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
+
+def DFT(signal):
+
+    return
+
+def IDFT(fourier_signal):
+
+    return
+
+signal = IDFT(fourier_signal)
+fourier_signal = DFT(signal)
