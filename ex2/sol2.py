988f072bcc309ffe09e37432af2fd2439481671c
itamakatz <itamakatz@gmail.com>
2016-12-09 16:04:50 +0200

    Started 1.2

--- ex2/sol2.py
+++ ex2/sol2.py
@@ -3,13 +3,29 @@ from scipy.misc import imread as imread
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 
-def DFT(signal):
+def General_DFT(x, mult):
+    # x - array to transform
+    # mult - 1 or -1 to define if DFT or IDFT
+    # return transform
 
-    return
+    N = signal.shape()
+    vander = np.vander(np.exp(np.arange(N)), increasing=True)
+    vect = np.exp(mult * 2 * np.pi * 1j * np.arange(N) / N)
+    vect = vect * signal
+    return vander.dot(vect)
+
+def DFT(signal):
+    return General_DFT(signal, -1)
 
 def IDFT(fourier_signal):
+    return General_DFT(signal, 1)
 
+def DFT2(image):
     return
+
+def IDFT2(image):
+    return
+
 
 signal = IDFT(fourier_signal)
 fourier_signal = DFT(signal)
