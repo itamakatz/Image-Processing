94e4c44f3c0a1f4f5b0eb77c119ad1c4c4f5ed7c
itamakatz <itamakatz@gmail.com>
2016-12-11 19:20:03 +0200

    Done with first try of code.  Now need to debug

--- ex2/sol2.py
+++ ex2/sol2.py
@@ -1,10 +1,12 @@
 import numpy as np
 from scipy.misc import imread as imread
+# from scipy.signal import convolve2d
 from scipy.signal import convolve2d
+import scipy.special
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 
-NUMERIC_ERROR = 14
+NUMERIC_ERROR = 13
 
 def General_DFT(x, mult):
     # x - array to transform
@@ -13,7 +15,7 @@ def General_DFT(x, mult):
 
     # compute vander matrix
     vander = np.vander((np.exp(mult * 2 * np.pi * 1j * np.arange(x.shape[0]) / x.shape[0])), increasing=True)
-    return np.around(vander.dot(x),14)
+    return np.around(vander.dot(x), NUMERIC_ERROR)
 
 def General_DFT2(x, mult):
     # x - array to transform
@@ -46,11 +48,38 @@ def conv_der(im):
     return np.sqrt(np.abs(xDer)**2 + np.abs(yDer)**2)
 
 def fourier_der(im):
-    vander_M = np.vander((np.exp(mult * 2 * np.pi * 1j * np.arange(x.shape[0]) / x.shape[0])), increasing=True)
-    vander_N = np.vander((np.exp(mult * 2 * np.pi * 1j * np.arange(x.shape[1]) / x.shape[1])), increasing=True)
-    return np.around(vander_M.dot(x.dot(vander_N)), NUMERIC_ERROR)
 
-im = np.arange(6)
+    vander_M = np.vander((np.exp(-1 * 2 * np.pi * 1j * np.arange(im.shape[0]) / im.shape[0])), increasing=True)
+    xDer =  np.around(vander_M.dot(np.arange(im.shape[0]) * im), NUMERIC_ERROR)
+
+    vander_N = np.vander((np.exp(-1 * 2 * np.pi * 1j * np.arange(im.shape[1]) / im.shape[1])), increasing=True)
+    yDer =  np.around((np.arange(im.shape[1]) * im).dot(vander_N), NUMERIC_ERROR)
+
+    return np.sqrt(np.abs(xDer) ** 2 + np.abs(yDer) ** 2)
+
+def create_ker(kernel_size):
+    bin = scipy.special.binom(kernel_size - 1, np.arange(kernel_size)).astype(np.int64)
+    kernel = convolve2d(bin[np.newaxis, :], bin[:, np.newaxis])
+    return kernel / np.sum(kernel)
+
+def blur_spatial (im, kernel_size):
+    return convolve2d(im, create_ker(kernel_size))
+
+def blur_fourier (im, kernel_size):
+    ker = create_ker(kernel_size)
+    ker_f = im * 0
+    center = (np.floor(im.shape[0] / 2), np.floor(im.shape[1] / 2))
+    ker_f[np.arange(center[0] - kernel_size,  center[0] + kernel_size), \
+          np.arange(center[1] - kernel_size,  center[1] + kernel_size)] = ker
+    ker_f = DFT2(np.copy(ker))
+    im_f = DFT2(im)
+    return IDFT2(im_f * ker_f)
+
+im = 0
+kernel_size = 3
+
+blur_im = blur_spatial (im, kernel_size)
+blur_im2 = blur_fourier (im, kernel_size)
 
 magnitude = fourier_der(im)
 magnitude2 = conv_der(im)
