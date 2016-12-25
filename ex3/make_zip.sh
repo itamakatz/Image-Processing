63b1dec827550cb327f26d963e86e4678ac232c1
itamakatz <itamakatz@gmail.com>
2016-12-25 22:04:24 +0200

    General - organized the dirs and files

--- ex3/make_zip.sh
+++ ex3/make_zip.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-cd /home/itamar/Documents/ip/ex2/files/presummition\ test/
-rm -f ex2.zip
-rm -f /home/itamar/Documents/ip/ex2/submition/sol2
-cp /home/itamar/Documents/ip/ex2/sol2.py /home/itamar/Documents/ip/ex2/submition/
+cd /home/itamar/Documents/ip/ex3/files/presubmition/
+rm -f ex3.zip
 
-cd /home/itamar/Documents/ip/ex2/submition/
-zip -r /home/itamar/Documents/ip/ex2/files/presummition\ test/ex2.zip ./*
+cd /home/itamar/Documents/ip/ex3/submition/
+zip -r /home/itamar/Documents/ip/ex3/files/presubmition/ex3.zip ./*
 
-
-cd /home/itamar/Documents/ip/ex2/files/presummition\ test/
-python3 ./test ./ex2
+cd /home/itamar/Documents/ip/ex3/files/presubmition/
+python3 ./test ./ex3
