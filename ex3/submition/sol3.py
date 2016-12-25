55418ffa2781b7eb53bab8cadd866bce2fa065cc
itamakatz <itamakatz@gmail.com>
2016-12-25 21:20:52 +0200

    ex3 - used some functional programing (map, functools.partial - carry function) to avoid for loops. may need to go back

--- ex3/submition/sol3.py
+++ ex3/submition/sol3.py
@@ -1,18 +1,19 @@
-import numpy as np
-from scipy.misc import imread
-from skimage.color import rgb2gray
 import os
-
-import matplotlib.pyplot as plt
-
-
+import functools
+import numpy as np
 import scipy.special
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
 from scipy.signal import convolve2d
-from scipy import ndimage
 
+# global parameter to plot as many figures as necessary
 g_plot_index = 1
 
 def index():
+    # simulates a static variables of g_plot_index.
+    # returns - number of figure before increment
+
     global g_plot_index
     g_plot_index += 1
     return g_plot_index - 1
@@ -29,11 +30,18 @@ def read_image(filename, representation):
     return (im / 255).astype(np.float32)
 
 def relpath(filename):
+    # converts relative paths to absolute
+    # filename - relative path
+    # returns - absolute path
+
     return os.path.join(os.path.dirname(__file__), filename)
 
-###########################################################################################################################3
 def create_filter_vec(filter_size):
+    # creates a binomial coefficient of length filter_size
+    # filter_size - length of the coefficient array
+    # returns - the binomial coefficient array
 
+    # special case of an odd number.
     if filter_size == 1: return np.array([[0]])
 
     conv_ker =  np.array([[1, 1]])
@@ -50,16 +58,21 @@ def create_filter_vec(filter_size):
         filter = convolve2d(filter, conv_ker).astype(np.float32)
 
     # normalize
-    return filter / np.sum(filter)
+    return (filter / np.sum(filter)).astype(np.float32)
 
 def build_gaussian_pyramid(im, max_levels, filter_size):
 
+    # calc the filter array
     filter_vec = create_filter_vec(filter_size)
 
-    pyr = [0] * (np.min([max_levels, np.log2(im.shape[0]).astype(np.int64) - 3, np.log2(im.shape[1]).astype(np.int64) - 3]))
+    # create the entire array for better complexity
+    pyr = [0] * (np.min([max_levels, np.log2(im.shape[0]).astype(np.int64) - 3,
+                         np.log2(im.shape[1]).astype(np.int64) - 3]))
 
     pyr[0] = im
 
+    # for each iter, use the last iter to calc the current iter. note i transpose twice. once to calc
+    # the y conv and the second to flip back the image
     for i in range(1, len(pyr)):
         pyr[i] = scipy.ndimage.filters.convolve(pyr[i - 1], filter_vec, output = None, mode = 'mirror')
         pyr[i] = scipy.ndimage.filters.convolve(pyr[i].transpose(), filter_vec, output = None, mode = 'mirror')
@@ -67,7 +80,12 @@ def build_gaussian_pyramid(im, max_levels, filter_size):
 
     return pyr, filter_vec
 
-def expand(im, filter_vec):
+def expand(filter_vec, im):
+    # method that helps calculate an expanded image given an image and a kernel array
+    # im - image to expand
+    # filter_vec - kernel used to build the gaussian pyramid
+    # return - the expanded image after interpolation
+
     expand = np.zeros([im.shape[0] * 2, im.shape[1] * 2], dtype=np.float32)
     expand[1::2, 1::2] = im
     expand = scipy.ndimage.filters.convolve(expand, filter_vec, output=None, mode='mirror')
@@ -76,33 +94,30 @@ def expand(im, filter_vec):
 
 def build_laplacian_pyramid(im, max_levels, filter_size):
 
-    gauss_pyr, filter_vec = build_gaussian_pyramid(im, max_levels + 1, filter_size)
-    filter_vec *= 2
+    gauss_pyr, filter_vec = build_gaussian_pyramid(im, max_levels, filter_size)
+    filter_vec *= 2 #  on expansion the kernel should not be completely normalized
+    pyr = [0] * len(gauss_pyr) # create the entire array for better complexity
 
-    # ============================== np.vectorize()
-    # pyr = [0] * len(gauss_pyr)
-    # v_expand = np.vectorize(expand, excluded=['filter_vecc'])
-    # expanded = v_expand(gauss_pyr[:-1], filter_vecc=filter_vec)
-    # pyr[:-1] = gauss_pyr[:-1] - expanded
-    # pyr[-1] = gauss_pyr[-1]
+    # using functional programing to avoid for loops
+    pyr[:-1] = np.ndarray.tolist(np.array(gauss_pyr[:-1]) -  \
+                                 np.array(list(map(functools.partial(expand, filter_vec), gauss_pyr[1:]))))
 
-
-    pyr = [0] * len(gauss_pyr)
-
-    for i in range(len(pyr) - 1):
-        pyr[i] = (gauss_pyr[i] - expand(gauss_pyr[i + 1], filter_vec)).astype(np.float32)
+    # for i in range(len(pyr) - 1):
+    #     pyr[i] = (gauss_pyr[i] - expand(filter_vec, gauss_pyr[i + 1])).astype(np.float32)
 
     pyr[-1] = gauss_pyr[-1]
     return pyr, filter_vec
 
 def laplacian_to_image(lpyr, filter_vec, coeff):
     im = np.array([[0]]).astype(np.float32)
-    for i  in range(len(lpyr) - 1):
-        im = expand(im + lpyr[-(i + 1)] * coeff[-(i + 1)], filter_vec)
-    return (im + lpyr[0] * coeff[0]).astype(np.float32)
 
-def calc_log_len(x, levels):
-    return
+    vfunc = functools.partial(expand, filter_vec)
+    pyr[:-1] = np.ndarray.tolist(np.array(gauss_pyr[:-1]) - np.array(list(map(vfunc, gauss_pyr[1:]))))
+
+    for i  in range(len(lpyr) - 1):
+        im = expand(filter_vec, im + lpyr[-(i + 1)] * coeff[-(i + 1)])
+
+    return (im + lpyr[0] * coeff[0]).astype(np.float32)
 
 def render_pyramid(pyr, levels):
     length = (pyr[0].shape[1] * 2 * (1 - 2**(-levels)))
@@ -126,7 +141,7 @@ def pyramid_blending(im1, im2, mask, max_levels, filter_size_im, filter_size_mas
     im2_lpyr, _ = build_laplacian_pyramid(im2, max_levels, filter_size_im)
     mask_gpyr, _ = build_gaussian_pyramid(mask.astype(np.float32), max_levels, filter_size_mask)
 
-    out_pyrl = np.array(mask_gpyr) * np.array(im1_lpyr) + (1 - np.array(mask_gpyr)) * np.array(im2_lpyr)
+    out_pyrl = (np.array(mask_gpyr) * np.array(im1_lpyr)) + (1 - np.array(mask_gpyr)) * np.array(im2_lpyr)
 
     return np.clip(laplacian_to_image(out_pyrl, filter_vec, np.ones(len(im1_lpyr))), 0, 1)
 
@@ -141,69 +156,32 @@ def examples(path_1, path_2, mask_path, max_levels, filter_size_im, filter_size_
     for i in range(3):
         im_blend[:,:,i] += pyramid_blending(im1[:,:,i], im2[:,:,i], mask, max_levels, filter_size_im, filter_size_mask)
 
+    plt.figure(index())
+
+    plt.subplot(221)
+    plt.imshow(im1)
+    plt.subplot(222)
+    plt.imshow(im2)
+    plt.subplot(223)
+    plt.imshow(mask, plt.cm.gray)
+    plt.subplot(224)
+    plt.imshow(im_blend)
+
+    plt.show()
 
     return im1, im2, mask, im_blend
 
 
 def blending_example1():
-    return examples('race2.jpg', 'givat2.jpg', 'mask2.jpg', 6, 3, 3)
+    return examples('race2.jpg', 'givat2.jpg', 'mask2.jpg', 6, 1, 3)
 
 def blending_example2():
     return examples("", "", "", 6, 3, 3)
 
 #
 
-im = read_image(relpath("givat2.jpg"), 1)
-max_levels, filter_size = 10, 3
-levels = 6
-
-
-pyr, filter_vec = build_gaussian_pyramid(im, max_levels, filter_size)
-# pyr, filter_vec = build_laplacian_pyramid(im, max_levels, filter_size)
-coeff = np.ones(len(pyr))
-re_im = laplacian_to_image(pyr, filter_vec, coeff)
-# display_pyramid(pyr, levels)
-
-# plt.figure(index())
-
-# for i in range(len(pyr)):
-
-    # half = len(pyr)  //  2 + 1
-    # plt.subplot(2,half,i + 1)
-    # plt.imshow(pyr[i], plt.cm.gray)
-
-# plt.figure(index())
-# plt.imshow(re_im, plt.cm.gray)
-
-# plt.show()
-
-# givat = read_image(relpath("givat2.jpg"), 1)
-# race = read_image(relpath("race2.jpg"), 1)
-# mask = read_image(relpath("mask2.jpg"), 1)
-
-filter_size_im, filter_size_mask = 3,7
-
-# mask *= 255
-# mask[mask > 0.5] = 1
-# mask[mask <= 0.5] = 0
-# im_blend = pyramid_blending(race, givat, mask, max_levels, filter_size_im, filter_size_mask)
-
-
-
-
 im1, im2, mask, im_blend = blending_example1()
 # im1, im2, mask, im_blend = blending_example2()
 
-plt.figure(index())
 
-plt.subplot(2,2,1)
-plt.imshow(im1)
-plt.subplot(2,2,2)
-plt.imshow(im2)
-plt.subplot(2,2,3)
-plt.imshow(mask, plt.cm.gray)
-plt.subplot(2,2,4)
-plt.imshow(im_blend)
-
-plt.show()
 
