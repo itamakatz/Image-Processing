8e12095a8c2db77f7eb67ad1296138c3b3fab97a
itamakatz <itamakatz@gmail.com>
2017-02-15 00:01:43 +0200

    ex5 - Done coding

--- ex5/submition/sol5.py
+++ ex5/submition/sol5.py
@@ -2,6 +2,7 @@ import numpy as np
 import sol5_utils as ut
 from scipy.misc import imread
 from keras.models import Model
+import matplotlib.pyplot as plt
 from keras.optimizers import Adam
 from scipy.ndimage import filters
 from skimage.color import rgb2gray
@@ -84,7 +85,12 @@ def restore_image(corrupted_image, base_model, num_channels):
     prediction = new_model.predict(corrupted_image[np.newaxis, np.newaxis] - 0.5)[0] + 0.5
     return np.clip(prediction,0,1).reshape(corrupted_image.shape)
 
-def learn(quick_mode, load_func, corr_func, model, ne):
+def add_gaussian_noise(image, min_sigma, max_sigma):
+
+    return np.clip(image + np.random.normal(scale = np.random.uniform(min_sigma, max_sigma), size = image.shape),
+                   0, 1)
+
+def learn_denoising_model(quick_mode = False):
 
     if quick_mode:
         batch_size = 10
@@ -94,21 +100,15 @@ def learn(quick_mode, load_func, corr_func, model, ne):
     else:
         batch_size = 100
         sam_per_epoch = 10000
-        num_epochs = ne
+        num_epochs = 5
         num_valid_sample = 1000
 
-    train_model(model, load_func(), corr_func, batch_size, sam_per_epoch, num_epochs, num_valid_sample)
-    return model
+    model = build_nn_model(24, 24, 48)
+    train_model(model, ut.images_for_denoising(),
+                lambda im: add_gaussian_noise(im, 0, 0.2),
+                batch_size, sam_per_epoch, num_epochs, num_valid_sample)
 
-def add_gaussian_noise(image, min_sigma, max_sigma):
-
-    return np.clip(image + np.random.normal(scale = np.random.uniform(min_sigma, max_sigma), size = image.shape),
-                   0, 1)
-
-def learn_denoising_model(quick_mode = False):
-
-    return learn(quick_mode, ut.images_for_denoising, lambda im: add_gaussian_noise(im, 0, 0.2),
-                         build_nn_model(24, 24, 48), 5), 48
+    return model, 48
 
 def add_motion_blur(image, kernel_size, angle):
 
@@ -120,5 +120,60 @@ def random_motion_blur(image, list_of_kernel_sizes):
 
 def learn_deblurring_model(quick_mode = False):
 
-    return learn(quick_mode, ut.images_for_deblurring, lambda im: random_motion_blur(im, [7]),
-                         build_nn_model(16, 16, 32), 10), 32
+    if quick_mode:
+        batch_size = 10
+        sam_per_epoch = 30
+        num_epochs = 2
+        num_valid_sample = 30
+    else:
+        batch_size = 100
+        sam_per_epoch = 10000
+        num_epochs = 10
+        num_valid_sample = 1000
+
+    model = build_nn_model(16, 16, 32)
+    train_model(model, ut.images_for_deblurring,
+                lambda im: random_motion_blur(im, [7]),
+                batch_size, sam_per_epoch, num_epochs, num_valid_sample)
+
+    return model, 32
+
+
+if __name__ == "__main__":
+
+    test = 1
+    if test == 1:
+        im = read_image("train/2092.jpg", 1)
+        noised_im = add_gaussian_noise(im, 0, 0.2)
+        plt.imshow(noised_im, cmap=plt.cm.gray)
+        plt.show()
+
+    if test == 2:
+        im = read_image("text_dataset/train/0000005_orig.png", 1)
+        noised_im = random_motion_blur(im, [7])
+
+        model, channels = learn_deblurring_model()
+        restored = restore_image(noised_im, model, channels)
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1, 2, 1, label="noise")
+        ax1.title.set_text("noise")
+        plt.imshow(noised_im, cmap=plt.cm.gray)
+        ax2 = fig.add_subplot(1, 2, 2)
+        ax2.title.set_text("restored")
+        plt.imshow(restored, cmap=plt.cm.gray)
+        plt.show()
+
+    if test == 3:
+        im = read_image("image_dataset/train/2092.jpg", 1)
+        noised_im = add_motion_blur(im, 0, 0.2)
+
+        model, channels = learn_denoising_model()
+        restored = restore_image(noised_im, model, channels)
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1, 2, 1, label="noise")
+        ax1.title.set_text("noise")
+        plt.imshow(noised_im, cmap=plt.cm.gray)
+        ax2 = fig.add_subplot(1, 2, 2)
+        ax2.title.set_text("restored")
+        plt.imshow(restored, cmap=plt.cm.gray)
+        plt.show()
